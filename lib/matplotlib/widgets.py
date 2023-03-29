7100b57770329467aa6400213a96a4c764edefd6
Thomas A Caswell <tcaswell@gmail.com>
2023-03-29 16:18:54 -0400

    FIX: Use more reliable way of checking the current figure's canvas
    closes #25572

--- lib/matplotlib/widgets.py
+++ lib/matplotlib/widgets.py
@@ -104,7 +104,10 @@ class Widget:
            True if the canvas has been changed.
 
         """
-        return self.canvas is not self.ax.figure.canvas
+        # no canvas, something is very fishy
+        if self.canvas is None:
+            return True
+        return self.canvas is not self.canvas.figure.canvas
 
 
 class AxesWidget(Widget):
